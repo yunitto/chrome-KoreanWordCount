12b6cfb50bad44e0487b3fbafbfd11dca3b180ae
yunitto <hyesooyoo.kr@gmail.com>
2018-12-23 13:42:41 +0900

    tmp

--- script.js
+++ script.js
@@ -4,32 +4,54 @@ var copyBtn = document.getElementById('copyBtn');
 
 window.onload = function(){
   chrome.storage.sync.get(function(saved){
-    saved_data = saved.data;
-    if (saved_data === undefined){
-      saved_data = '';
+    savedData = saved.data;
+    if (savedData === undefined){
+      savedData = '';
     }
-    userInput.value = saved_data;
-    countWords(saved_data);
+    userInput.value = savedData;
+    countWords(savedData);
     textAreaResize();
   });
 };
 
-function countWords(user_words){
-  var length = user_words.length;
-  document.getElementById('result').innerText = length + ' 자';
+function countWords(userWords){
+  var length = userWords.length;
+  var lengthNoSpace = userWords.replace(/\s/g,'').length;
+  document.getElementById('result-space').innerText = '공백 포함 ' + length + ' 자';
+  document.getElementById('result-no-space').innerText = '공백 제외 ' + lengthNoSpace + ' 자';
 };
 
 function textAreaResize(){
-  var text_area = document.getElementById('userInput');
-  text_area.style.height = 'auto';
-  text_area.style.height = (text_area.scrollHeight) + 'px';
+  var textArea = document.getElementById('userInput');
+  textArea.style.height = 'auto';
+  textArea.style.height = (textArea.scrollHeight) + 'px';
   chrome.storage.sync.set({
     'data': userInput.value
    });
 };
 
+function syncStorage(userInput){
+  var segments = {}
+  var k = 0, v, idx = 0;
+  while(userInput > 0){
+    var length = chrome.storage.sync.QUOTA_BYTES_PER_ITEM - k.toString.length - 1;
+    v = userInput.substring(idx, idx + length);
+    var overflow = JSON.stringify(v).length - chrome.storage.sync.QUOTA_BYTES_PER_ITEM
+    if (overflow > 0){
+      length = length - overflow
+    }
+
+    segments[k] = v;
+    userInput = userInput.substring(idx + length + 1)
+    k++;
+  }
+  chrome.storage.sync.set(segments)
+}
+
 clearBtn.addEventListener('click', function(event){
-  userInput.value = '';
+  var deleteText = document.getElementById('userInput')
+  deleteText.select()
+  document.execCommand("delete")
   countWords(userInput.value);
   textAreaResize();
 });
