1c08ce2eff2f75fdb245083de97671cbb74f0c73
Yi Hyunjoon <yi.hyunjoon@gmail.com>
2018-12-23 16:26:52 +0900

    Format script

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 var userInput = document.getElementById('user-input');
-var clearBtn = document.getElementById('clear-button');
-var copyBtn = document.getElementById('copy-button');
+var clearButton = document.getElementById('clear-button');
+var copyButton = document.getElementById('copy-button');
 
-window.onload = function(){
-  chrome.storage.sync.get(function(saved){
+copyButton.addEventListener('click', function (event) {
+  userInput.select();
+  document.execCommand('Copy');
+});
+
+clearButton.addEventListener('click', function (event) {
+  userInput.select();
+  document.execCommand('delete');
+  countWords(userInput.value);
+  textAreaResize();
+});
+
+userInput.oninput = function () {
+  countWords(userInput.value);
+  textAreaResize();
+};
+
+window.onload = function () {
+  chrome.storage.sync.get(function (saved) {
     savedData = saved.data;
-    if (savedData === undefined){
+    if (savedData === undefined) {
       savedData = '';
     }
     userInput.value = savedData;
@@ -14,17 +31,17 @@ window.onload = function(){
   });
 };
 
-function countWords(userWords){
+function countWords(userWords) {
   var length = userWords.length;
   var lengthNoSpace = userWords.replace(/\s/g,'').length;
-  document.getElementById('result-space').innerText = '공백 포함 ' + length + ' 자';
-  document.getElementById('result-no-space').innerText = '공백 제외 ' + lengthNoSpace + ' 자';
+  document.getElementById('result-space').innerText = `공백 포함 ${length} 자`;
+  document.getElementById('result-no-space').innerText = `공백 제외 ${lengthNoSpace} 자`;
 };
 
-function textAreaResize(){
+function textAreaResize() {
   var textArea = document.getElementById('user-input');
   textArea.style.height = 'auto';
-  textArea.style.height = (textArea.scrollHeight) + 'px';
+  textArea.style.height = `${textArea.scrollHeight}px`;
   chrome.storage.sync.set({
     'data': userInput.value
    });
@@ -46,23 +63,4 @@ function syncStorage(userInput){
     k++;
   }
   chrome.storage.sync.set(segments)
-}
-
-clearBtn.addEventListener('click', function(event){
-  var deleteText = document.getElementById('user-input')
-  deleteText.select()
-  document.execCommand("delete")
-  countWords(userInput.value);
-  textAreaResize();
-});
-
-copyBtn.addEventListener('click', function(event){
-  var copyText = document.getElementById('user-input')
-  copyText.select();
-  document.execCommand("Copy");
-});
-
-userInput.oninput = function(){
-  countWords(userInput.value);
-  textAreaResize();
 };
